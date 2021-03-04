4e2ae2efe0be43edcd59fbb3bf49dc22bc8b9a3f
calin522 <calin.ardeleanu522@gmail.com>
2021-03-04 12:21:58 +0200

    Commit 3

---
+++ seminar2/src/ro/ase/cts/clase/UniversalReader.java
@@ -0,0 +1,16 @@
+package ro.ase.cts.clase;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class UniversalReader {
+	//TO DO
+// punct de legatura catre readere
+}
