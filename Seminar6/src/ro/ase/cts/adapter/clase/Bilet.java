c9691f355f1f1b56e216871ff6913533f5f17bba
calin522 <calin.ardeleanu522@gmail.com>
2021-04-01 13:17:32 +0300

    Commit Adapter

---
+++ Seminar6/src/ro/ase/cts/adapter/clase/Bilet.java
@@ -0,0 +1,18 @@
+package ro.ase.cts.adapter.clase;
+
+public class Bilet {
+	private float pret;
+
+	public Bilet(float pret) {
+		super();
+		this.pret = pret;
+	}
+	
+	public void rezerva() {
+		System.out.println("A fost rezervat biletul cu pretul de " + pret);
+	}
+	
+	public void vinde() {
+		System.out.println("A fost vandut biletul cu pretul de " + pret);
+	}
+}
---
+++ Seminar6/src/ro/ase/cts/adapter/clase/BiletOnline.java
@@ -0,0 +1,7 @@
+package ro.ase.cts.adapter.clase;
+
+public interface BiletOnline {
+	public void vindeBilet();
+	public void rezervaBilet();
+	
+}
