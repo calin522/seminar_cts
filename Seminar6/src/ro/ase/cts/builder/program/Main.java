d7324ae1f66224c26449c17f8bc4469e40962167
calin522 <calin.ardeleanu522@gmail.com>
2021-04-01 12:49:57 +0300

    Commit BuilderV2

--- Seminar6/src/ro/ase/cts/builder/program/Main.java
+++ Seminar6/src/ro/ase/cts/builder/program/Main.java
@@ -1,5 +1,6 @@
 package ro.ase.cts.builder.program;
 
+import ro.ase.cts.builder.clase.BuilderRezervareV2;
 import ro.ase.cts.builder.clase.Rezervare;
 import ro.ase.cts.builder.clase.RezervareBuilder;
 
@@ -11,5 +12,13 @@ public class Main {
 
 		System.out.println(rezervare1);
 		System.out.println(rezervare2);
+		
+		
+		BuilderRezervareV2 builder2= new BuilderRezervareV2();
+		Rezervare rezervare3 = builder2.setAreBauturaInclusa(true).build();
+		Rezervare rezervare4 = builder2.setAreMuzicaAmbientala(true).setGenMuzical("trap").build();
+		
+		System.out.println(rezervare3.toString());
+		System.out.println(rezervare4.toString());
 	}
 }
