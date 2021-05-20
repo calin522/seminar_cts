283da0cdeb590c33687b85a230fc200c225084a8
calin522 <calin.ardeleanu522@gmail.com>
2021-05-20 13:06:02 +0300

    commit test 2

--- seminar12/src/ro/ase/cts/pachet/clase/Student.java
+++ seminar12/src/ro/ase/cts/pachet/clase/Student.java
@@ -8,7 +8,6 @@ public class Student {
 	private List<Integer> note;
 
 	public Student(String nume) {
-		// this.nume = nume;
 		this.nume = nume;
 		this.note = new ArrayList<>();
 	}
@@ -31,7 +30,11 @@ public class Student {
 	}
 
 	public void adaugaNota(int nota) {
-		note.add(nota);
+		if (nota < 1 || nota <= 10) {
+			note.add(nota);
+		} else {
+			throw new IllegalArgumentException();
+		}
 	}
 
 	public int getNota(int index) {
--- seminar12/src/ro/ase/cts/pachet/tests/StudentTests.java
+++ seminar12/src/ro/ase/cts/pachet/tests/StudentTests.java
@@ -62,4 +62,51 @@ public class StudentTests {
 
 	}
 
+	@Test
+	public void testCalculeazaMedia() {
+		Student student = new Student();
+		student.adaugaNota(5);
+		student.adaugaNota(6);
+		assertEquals(5.5, student.calculeazaMedie(), 0.01);
+	}
+
+	@Test
+	public void testCalculeazaMedieFaraNote() {
+		Student student = new Student();
+
+		assertEquals(0, student.calculeazaMedie(), 0);
+	}
+
+	@Test
+	public void testCalucleazaMediePentruONota() {
+		Student student = new Student();
+		int nota = 7;
+		student.adaugaNota(nota);
+
+		assertEquals(nota, student.calculeazaMedie(), 0);
+	}
+
+	@Test
+	public void testAdaugaNotaIncorecta() {
+		Student student = new Student();
+		int nota = 14;
+		// 1
+		try {
+			// 2
+			student.adaugaNota(nota);
+			// 3
+			fail("nu trebuia sa ajunga aici");
+		} catch (IllegalArgumentException err) {
+			// 4
+		}
+		// 5
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testAdaugaNotaIncorectaJU4() {
+		Student student = new Student();
+		int nota = 14;
+		student.adaugaNota(nota);
+	}
+
 }
